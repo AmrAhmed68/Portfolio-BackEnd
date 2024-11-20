75188fec1e3aefbd4dd79e0fe2538f147ea1b773
AmrAhmed68 <amrahmed682003@gmail.com>
2024-11-21 01:37:46 +0200

    Amr

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -3,42 +3,30 @@ const router = express.Router();
 const Project = require('../models/project');
 
 router.get('/project' , async (req , res) => {
-        res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-seven-opal-43.vercel.app'); // Allow only this origin
-         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Specify allowed methods
-         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify allowed headers
-         try {
-             const project = await Project.find();
-             res.json(project);
-            } catch (error) {
-                res.status(500).json({ message : error.message})
-            }
-            res.json({ message: 'Hello from API!' });
+    try {
+        const project = await Project.find();
+        res.json(project);
+        } catch (error) {
+            res.status(500).json({ message : error.message})
+        }
 })
 
 router.get('/:id', async (req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-seven-opal-43.vercel.app'); // Allow only this origin
-         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Specify allowed methods
-         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify allowed headers
-
-         try {
-             const project = await Project.findById(req.params.id);
-             if (!project) {
-                 return res.status(404).json({ error: 'Project not found' });
-                }
-                res.json(project);
-            } catch (error) {
-                res.status(500).json({ error: 'Server error' });
-            }
-            res.json({ message: 'Hello from API!' });
+    try {
+      const project = await Project.findById(req.params.id);
+      if (!project) {
+        return res.status(404).json({ error: 'Project not found' });
+      }
+      res.json(project);
+    } catch (error) {
+      res.status(500).json({ error: 'Server error' });
+    }
 });
   
 router.post('/project' , async (req , res) => {
-        res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-seven-opal-43.vercel.app'); // Allow only this origin
-         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Specify allowed methods
-         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify allowed headers
-
-         const project = new Project ({
-             title : req.body.title,
+    
+    const project = new Project ({
+        title : req.body.title,
         description : req.body.description,
         imageUrl : req.body.imageUrl,
         video : req.body.video,
@@ -52,7 +40,6 @@ router.post('/project' , async (req , res) => {
     }catch (error){
         res.status(400).json({ message : error.message})
     }
-    res.json({ message: 'Hello from API!' });
 })
 
 
--- backend/server.js
+++ backend/server.js
@@ -15,41 +15,13 @@ app.use(
   cors({
     origin: ["https://portfolio-seven-opal-43.vercel.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
 
-const allowedOrigins = [
-  'https://portfolio-seven-opal-43.vercel.app'
-];
-
-// Middleware for CORS
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
-  res.header('Access-Control-Allow-Credentials', 'true'); // If you need to send cookies
-  next();
-});
-
-// Add a preflight OPTIONS route
-app.options('*', (req, res) => {
-  res.sendStatus(204);
-});
-
-
 app.use("/api" ,  adminRoutes  );
 app.use("/api", projectRoutes );
 app.use("/api", skillsRoutes  );
-
-
-app.use("/secure-route" , (req, res) => {
-  res.json({ message: "You have access!" });
-});
 
 mongoose
   .connect(process.env.MONGO_URL, {
