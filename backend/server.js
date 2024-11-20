866771d167691aa81d5833000b7719b9d34ffd18
AmrAhmed68 <amrahmed682003@gmail.com>
2024-11-20 23:02:07 +0200

    Amr

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://portfolio-back-end-rust.vercel.app" ],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
