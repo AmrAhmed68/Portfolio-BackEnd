8e38a97cebe6588da4c59dd101a643b0a5abadb9
AmrAhmed68 <amrahmed682003@gmail.com>
2024-11-21 00:47:04 +0200

    Amr

--- frontend/src/services/projectCardService.js
+++ frontend/src/services/projectCardService.js
@@ -7,52 +7,26 @@ const API_URL_PROJECTID = `${API_URL}/api/`
 const API_URL_SKILLS = `${API_URL}/api/skill`
 
 export const getProject = async () => {
-  const token = localStorage.getItem("token"); 
-  const response = await axios.get(API_URL_PROJECT , {
-    headers: {
-      Authorization: `Bearer ${token}`, 
-    },
-  });
+  const response = await axios.get(API_URL_PROJECT);
   return response.data;
 };
 
 export const getProjectById = async (id) => {
-  const token = localStorage.getItem("token");
-  const response = await axios.get(API_URL_PROJECTID , {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      },
-  });
+  const response = await axios.get(API_URL_PROJECTID);
   return response.data;
 };
 
 export const addProject = async (project) => {
-  const token = localStorage.getItem("token");
-  const response = await axios.post(API_URL_PROJECT, project ,{
-    headers: {
-      Authorization: `Bearer ${token}`,
-      },
-  });
+  const response = await axios.post(API_URL_PROJECT, project);
   return response.data;
 };
 
 export const getSkills = async () => {
-  const token = localStorage.getItem("token")
-  const response = await axios.get(API_URL_SKILLS , {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      },
-  });
+  const response = await axios.get(API_URL_SKILLS);
   return response.data;
 };
 
-
 export const addSkills = async (skills) => {
-  const token = localStorage.getItem("token")
-  const response = await axios.post(API_URL_SKILLS, skills , {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      },
-  });
+  const response = await axios.post(API_URL_SKILLS, skills);
   return response.data;
 };
